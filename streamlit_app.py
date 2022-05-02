dec5eca223ef41697cd093f815bf249b7bbb9a1f
yasaswini <104744068+yasaswinimadhu@users.noreply.github.com>
2022-05-02 08:55:43 +0530

    Update streamlit_app.py
    streamlit.header('Breakfast Favorites')
    streamlit.text('Omega 3 & Blueberry Oatmeal')
    streamlit.text('Kale, Spinach & Rocket Smoothie')
    streamlit.text('ard-Boiled Free-Range Egg') 
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit;
 streamlit.title('My MOM\'S New Healthy diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('kale,Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+

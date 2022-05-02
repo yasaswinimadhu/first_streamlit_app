84f8334578398a6473fd1f9327a355c957b5c6aa
yasaswini <104744068+yasaswinimadhu@users.noreply.github.com>
2022-05-02 08:45:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit;
 streamlit.title('My MOM\'S New Healthy diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('kale,Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Freerange-egg')
